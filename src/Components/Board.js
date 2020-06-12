ed6c5ef4816c6134b91ddf7e386efb9b62f3e955
anshusaurav <anshu.saurav@gmail.com>
2020-06-12 19:07:27 +0530

    Add dark red buttons

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -7,9 +7,21 @@ class Board extends React.Component{
         
     }
     render(){
+        let redColorArr = [{x: 0, y: 0},{x: 7, y: 0},{x: 14, y: 0},{x: 0, y: 7},{x: 7, y: 14},{x: 0, y: 14},{x: 7, y: 14},{x: 14, y: 14}];
         let cells = [];
-        for(let i = 0; i < 15*15; i++) {
-            cells.push(<Cell/>);    
+        for(let i = 0; i < 15; i++) {
+            for(let j = 0; j < 15; j++) {
+                let index = -1;
+                redColorArr.forEach((elem,ind) =>{
+                    if(elem.x ===i && elem.y ===j)
+                        index = ind;
+                })
+                console.log(i, j, index);
+                if(index === -1)
+                    cells.push(<Cell/>);  
+                else
+                    cells.push(<Cell bgTag='red'/>);  
+            }
         }
         return (
             <div className='game-container'>
--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -3,12 +3,15 @@ import React from 'react';
 class Cell extends React.Component{
     constructor(props) {
         super(props);
-        this.state = { multiplier: 1, isLetterPresent:'false', 'letterPresent': '', bgTag:''}
     }
     render(){
         return (
             <div className='board-cell'>
-                
+                {this.props.bgTag > 0 && 
+                    <p>
+                        {this.props.bgTag}
+                    </p>
+                }
             </div>
         );
     }
